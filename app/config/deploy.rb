82daa7ba1cb62d737253a7beff5a4e345529c725
Thewolfs <Thewolfs@users.github.com>
2016-02-25 23:03:18 +0100

    Nouveau deploy

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,9 +1,9 @@
 set :application, "decibels"
-set :domain,      "files.mde.utc"
-set :deploy_to,   "public_html"
+set :domain,      "decibels@files.mde.utc"
+set :deploy_to,   "/sites/decibels"
 set :app_path,    "app"
 
-set :repository,  "git@github.com:Thewolfs/Decibels.git"
+set :repository,  "git@github.com:Thewolfs/decibelsUTC.git"
 set :scm,         :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
@@ -15,14 +15,10 @@ role :app,        domain, :primary => true       # This may be the same as your 
 
 set  :keep_releases,  3
 
-set  :shared_files, ["app/config/parameters.yml"]
-set  :shared_children, [app_path + "/logs", web_path + "/uploads", "vendors"]
-
-set  :use_composer, true
-
-set  :user, "decibels"
-
 set  :use_sudo, false
 
+set  :update_vendors, true
+
+set :shared_children, [app_path + "/logs", web_path + "/uploads", "vendor"]
 # Be more verbose by uncommenting the following line
 # logger.level = Logger::MAX_LEVEL
