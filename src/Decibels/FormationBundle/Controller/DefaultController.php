fee4bfc442bb5ff561230e5460952ad67285d050
Thewolfs <gomen565@gmail.com>
2016-04-23 18:37:59 +0200

    Delete Action

--- src/Decibels/FormationBundle/Controller/DefaultController.php
+++ src/Decibels/FormationBundle/Controller/DefaultController.php
@@ -7,6 +7,7 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Request;
 use Decibels\FormationBundle\Entity\Formation;
 use Decibels\FormationBundle\Form\FormationType;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
@@ -31,6 +32,10 @@ class DefaultController extends Controller
 		if($form->handleRequest($request)->isValid())
 		{
 			$em = $this->getDoctrine()->getManager();
+			$file = $em->getRepository('DecibelsGeneralBundle:File')->find($request->request->get('file_id'));
+			$formation->setFile($file);
+			$date = new \DateTime();
+			$formation->setDate($date);
 			$em->persist($formation);
 			$em->flush();
 			
@@ -58,19 +63,58 @@ class DefaultController extends Controller
 			throw new NotFoundHttpException("La formation spécifié n'existe pas");
 		}
 		
-		$form = $this->get('form.factory')->create(new MaterielType, $formation);
+		$form = $this->get('form.factory')->create(new FormationType, $formation);
 		
 		if($form->handleRequest($request)->isValid())
 		{
+			if($request->request->get('file_id')) 
+			{
+				$file = $em->getRepository('DecibelsGeneralBundle:File')->find($request->request->get('file_id'));
+				$formation->setFile($file);
+			}
 			$em->flush();
 			
-			$request->getSession()->getFlashBag()->add('notice', 'Formation bien modifié.');
+			$request->getSession()->getFlashBag()->add('notice', 'Formation bien modifiée.');
 			 
 			return $this->redirect($this->generateUrl('decibels_formation_list'));
 		}
 		
 		return $this->render('DecibelsFormationBundle:Default:edit.html.twig', array(
 			'form' => $form->createView()
+		));
+	}
+	
+	public function deleteAction($id, Request $request)
+	{
+		if (false === $this->get('security.authorization_checker')->isGranted('ROLE_ADMIN'))
+		{
+			throw new AccessDeniedException();
+		}
+		
+		$em = $this->getDoctrine()->getManager();
+		
+		$formation = $em->getRepository('DecibelsFormationBundle:Formation')->find($id);
+		
+		if(null === $formation)
+		{
+			throw new NotFoundHttpException("La formation spécifié n'existe pas");
+		}
+		
+		$form = $this->createFormBuilder()->getForm();
+		
+		if($form->handleRequest($request)->isValid())
+		{
+			$em->remove($formation);
+			$em->flush();
+			
+			$request->getSession()->getFlashBag()->add('notice', 'Formation bien supprimée.');
+			 
+			return $this->redirect($this->generateUrl('decibels_formation_list'));
+		}
+		
+		return $this->render('DecibelsFormationBundle:Default:delete.html.twig', array(
+			'form' => $form->createView(),
+			'formation' => $formation
 		));
 	}
 }
